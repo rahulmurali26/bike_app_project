1b2f08e9ed9e9e2de92b5bf488992f6af9c3b930
Eli Young <eli.young@ucdconnect.ie>
2025-02-17 10:35:00 +0000

    my version of jcdecaux
    not using dbinfo file, included error handling

---
+++ jcdecauxV2.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+API_KEY = "32445a2ba7fec842626d4eb1b5248fe4f0ac3d9d"
+CITY_NAME = "dublin"
+STATIONS_URI = "https://api.jcdecaux.com/vls/v1/stations"
+
+response = requests.get(STATIONS_URI, params={"apiKey": API_KEY, "contract": CITY_NAME})
+
+if response.status_code == 200:  #Check if request was successful
+    data = json.loads(response.text)
+    print(json.dumps(data, indent=4))
+else:
+    print("Error:", response.status_code)  # Handle errors
