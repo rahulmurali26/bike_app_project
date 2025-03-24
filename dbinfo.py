b8625b546df272e912bcfc5e963263385c07b606
GeraldJ99 <gerald.jalagna@ucdconnect.ie>
2025-03-24 13:48:37 +0000

    Update dbinfo.py

--- dbinfo.py
+++ dbinfo.py
@@ -1,5 +1,6 @@
 JCKEY = "d918102f9563a7c1bb20540841c898456cbfc3f4"
 NAME = "dublin"
 STATION_URI = "https://api.jcdecaux.com/vls/v1/stations"
+#Gerald's Google Map API
 MAP_API='AIzaSyBOC_l8DI0Lc3pe8cqPqcNLufkHpqpDEVI'
 
