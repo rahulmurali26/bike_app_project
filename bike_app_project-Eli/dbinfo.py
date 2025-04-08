20a469acf8ec27d80c5b4ac085c21f2096b4105b
Eli Young <140007889+elicryoung@users.noreply.github.com>
2025-04-08 20:31:10 +0100

    Updated front end

---
+++ bike_app_project-Eli/dbinfo.py
@@ -0,0 +1,11 @@
+<<<<<<< HEAD
+JCKEY = "yourapiKey"
+NAME = "dublin"
+STATION_URI = "https://api.jcdecaux.com/vls/v1/stations"
+=======
+JCKEY = "7f53d1d974a374e40dfa9c54024728b0e13e8ce5"
+NAME = "dublin"
+STATION_URI = "https://api.jcdecaux.com/vls/v1/stations"
+MAP_API='AIzaSyBOC_l8DI0Lc3pe8cqPqcNLufkHpqpDEVI'
+Weather_API='72e7bbdfdba07b96a4290bf1506742af'
+>>>>>>> bb6317f (Updated front end)
