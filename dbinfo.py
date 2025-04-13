4c02946c4d31c4e9f846726bc836fec9f83a09fb
Rahul Murali <rahul.murali@ucdconnect.ie>
2025-04-13 14:02:17 +0100

    Update dbinfo.py
    Hidding the keys from public github repository

--- dbinfo.py
+++ dbinfo.py
@@ -1,5 +1,5 @@
-JCKEY = "d918102f9563a7c1bb20540841c898456cbfc3f4"
+JCKEY = "JCDECAUX_API_KEY"
 NAME = "dublin"
 STATION_URI = "https://api.jcdecaux.com/vls/v1/stations"
-MAP_API = 'AIzaSyBOC_l8DI0Lc3pe8cqPqcNLufkHpqpDEVI'
-Weather_API = '72e7bbdfdba07b96a4290bf1506742af'
+MAP_API = 'GOOGLE_MAPS_API_KEY'
+Weather_API = 'OPEN_WEATHER_API_KEY'
